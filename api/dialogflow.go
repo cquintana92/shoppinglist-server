1a8543baeaf4948db011630c0d6fa08de23bb66f
Carlos Quintana <carlos@cquintana.dev>
2022-09-05 11:36:02 +0200

    Dialogflow fixes

--- api/dialogflow.go
+++ api/dialogflow.go
@@ -57,6 +57,36 @@ type DialogFlowResponseSimpleResponse struct {
 	TextToSpeech string `json:"textToSpeech"`
 }
 
+func processDialogFlowRequest(body []byte) string {
+	request := &DialogFlowRequest{}
+	err := json.Unmarshal(body, request)
+	if err != nil {
+		log.Logger.Errorf("Error reading DialogFlow as JSON: %+v", err)
+		return "No he podido hacer lo que me has pedido porque ha habido un error"
+	}
+
+	log.Logger.Debugf("%+v", request)
+	ingredient := request.QueryResult.Parameters.Ingredient
+	log.Logger.Infof("DialogFlow ingredient: %s", ingredient)
+
+	if ingredient == "" {
+		return "Perdona, no he entendido qué es lo que quieres que apunte. Igual ha habido un error"
+	}
+
+	err = storage.New(ingredient)
+	if err != nil {
+		if err == storage.ItemAlreadyExistsError {
+			log.Logger.Warnf("Item [%s] already exists", ingredient)
+			return fmt.Sprintf("No he añadido %s porque ya estaba", ingredient)
+		} else {
+			log.Logger.Errorf("Error creating a new record: %+v", err)
+			return fmt.Sprintf("No he podido añadir %s porque ha habido un error.", ingredient)
+		}
+	} else {
+		return fmt.Sprintf("Acabo de añadir %s a tu lista de la compra", ingredient)
+	}
+}
+
 func dialogFlowHandler(w http.ResponseWriter, r *http.Request) {
 	log.Logger.Info("Received DialogFlow request")
 
@@ -67,30 +97,11 @@ func dialogFlowHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	request := &DialogFlowRequest{}
-	err = json.Unmarshal(body, request)
-	if err != nil {
-		log.Logger.Errorf("Error reading DialogFlow as JSON: %+v", err)
-		return
-	}
+	// We must always return a response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
 
-	log.Logger.Debugf("%+v", request)
-	ingredient := request.QueryResult.Parameters.Ingredient
-	log.Logger.Infof("DialogFlow ingredient: %s", ingredient)
-
-	err = storage.New(ingredient)
-	var dialogFlowResponse string
-	if err != nil {
-		if err == storage.ItemAlreadyExistsError {
-			log.Logger.Warnf("Item [%s] already exists", ingredient)
-			dialogFlowResponse = fmt.Sprintf("No he añadido %s porque ya estaba", ingredient)
-		} else {
-			log.Logger.Errorf("Error creating a new record: %+v", err)
-			dialogFlowResponse = fmt.Sprintf("No he podido añadir %s porque ha habido un error. Carlos, te toca mirarlo", ingredient)
-		}
-	} else {
-		dialogFlowResponse = fmt.Sprintf("Acabo de añadir %s a tu lista de la compra", ingredient)
-	}
+	dialogFlowResponse := processDialogFlowRequest(body)
 
 	res := &DialogFlowResponse{
 		FulfilmentMessages: []DialogFlowFulfilmentMessage{
@@ -118,7 +129,5 @@ func dialogFlowHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
 	w.Write(asBytes)
 }
